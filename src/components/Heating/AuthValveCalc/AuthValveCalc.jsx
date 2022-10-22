ebab79687b8abe86ae2f257d92b37d26c754d74b
Bartosz Kilinski <bartosz.kilinski@schibsted.com>
2022-10-22 15:13:40 +0200

    Auth

--- src/components/Heating/AuthValveCalc/AuthValveCalc.jsx
+++ src/components/Heating/AuthValveCalc/AuthValveCalc.jsx
@@ -1,14 +1,29 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Grid, Paper, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import ValveKvCalc from './ValveKvCalc'
 import ValvePressureDrop from './ValvePressureDrop'
 
-const AuthValveCalc = () => (
-  <Grid container spacing={1}>
-    <ValveKvCalc />
-    <ValvePressureDrop />
-    {/* TODO figure out calc about true authority of the valve using previous calcs */}
-  </Grid>
-)
+const paperSX = { py: 1, px: 2 }
+const typograhySX = { fontSize: '0.8rem', textTransform: 'uppercase', mb: 2, mt: 1 }
+
+const AuthValveCalc = () => {
+  const [calcValvePrDrop, setCalcValvePrDrop] = useState('')
+  const [trueValvePrDrop, setTrueValvePrDrop] = useState('')
+  const a = 5
+  return (
+    <Grid container spacing={1}>
+      <ValveKvCalc setCalcValvePrDrop={setCalcValvePrDrop} />
+      <ValvePressureDrop setTrueValvePrDrop={setTrueValvePrDrop} />
+      <Grid item xs={12}>
+        <Paper elevation={2} sx={paperSX}>
+          <Typography sx={typograhySX}>Spadek ciśnienia na zaworze</Typography>
+          <pre>{JSON.stringify(calcValvePrDrop, null, 2)}</pre>
+          <pre>{JSON.stringify(trueValvePrDrop, null, 2)}</pre>
+        </Paper>
+        {/* TODO figure out calc about true authority of the valve using previous calcs */}
+      </Grid>
+    </Grid>
+  )
+}
 
 export default AuthValveCalc
